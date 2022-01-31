037b0f14798f9e5d2821ddfbf8b8bac626f97a49
WesternGamer <80211714+WesternGamer@users.noreply.github.com>
2022-01-31 12:34:41 -0500

    Update AssemblyInfo.cs

--- ClientPlugin/Properties/AssemblyInfo.cs
+++ ClientPlugin/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ClientConsole")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides console attached to Space Engineers for debugging purposes.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WesternGamer")]
 [assembly: AssemblyProduct("ClientPluginClientConsole")]
